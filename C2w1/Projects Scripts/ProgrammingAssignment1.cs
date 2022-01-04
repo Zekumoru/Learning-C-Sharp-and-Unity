a3b62b30e9c43d92a29cd80061926d912afc4929
Zekumoru Dragonhart <zekumorudoragonhatto@gmail.com>
2022-01-04 23:40:25 +0100

    Added project assignment 1 solution

---
+++ C2w1/Projects Scripts/ProgrammingAssignment1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ProgrammingAssignment1
+{
+    // IMPORTANT: Only add code in the section
+    // indicated below. The code I've provided
+    // makes your solution work with the 
+    // automated grader on Coursera
+
+    /// <summary>
+    /// Programming Assignment 1
+    /// </summary>
+    class Program
+    {
+        // number to classify
+        static int number;
+
+        /// <summary>
+        /// Programming Assignment 1
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            // loop while there's more input
+            string input = Console.ReadLine();
+            while (input[0] != 'q')
+            {
+                // extract number from string
+                GetInputValueFromString(input);
+
+                // Add your code between this comment
+                // and the comment below. You can of
+                // course add more space between the
+                // comments as needed
+
+                // check if even or odd
+                if ((number % 2) == 0)
+                {
+                    Console.Write("e");
+                }
+                else
+                {
+                    Console.Write("o");
+                }
+
+                // print a space
+                Console.Write(" ");
+
+                // print -1 if negative, 1 if positive, or zero if 0
+                if (number == 0)
+                {
+                    Console.Write("0");
+                }
+                else if (number > 0)
+                {
+                    Console.Write("1");
+                }
+                else
+                {
+                    Console.Write("-1");
+                }
+
+                // print new line
+                Console.WriteLine();
+
+                // Don't add or modify any code below
+                // this comment
+                input = Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Extracts the number from the given input string
+        /// </summary>
+        /// <param name="input">input string</param>
+        static void GetInputValueFromString(string input)
+        {
+            number = int.Parse(input);
+        }
+    }
+}
