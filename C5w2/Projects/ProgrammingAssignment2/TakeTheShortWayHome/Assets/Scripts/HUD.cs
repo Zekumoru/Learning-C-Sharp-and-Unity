13608989a2acd6131aa9fe2ce5371f8cb0729859
Zekumoru Dragonhart <zekumorudoragonhatto@gmail.com>
2022-02-01 23:27:30 +0100

    Added C5w2 assignment

---
+++ C5w2/Projects/ProgrammingAssignment2/TakeTheShortWayHome/Assets/Scripts/HUD.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// The HUD
+/// </summary>
+public class HUD : MonoBehaviour
+{
+    [SerializeField]
+    Text pathLengthText;
+
+    const string PathLenghtTextPrefix = "Path Length: ";
+
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Awake()
+	{
+        EventManager.AddPathFoundListener(SetPathLength);
+	}
+
+    /// <summary>
+    /// Sets the path length in the hud
+    /// </summary>
+    /// <param name="length">path length</param>
+    void SetPathLength(float length)
+    {
+        if (length == float.MaxValue)
+        { 
+            pathLengthText.text = "Path not found!";
+            return;
+        }
+        pathLengthText.text = PathLenghtTextPrefix + length.ToString();
+    }
+}
